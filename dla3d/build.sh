cd2f540680e666f4cec56e430f36b492698cafec
Theo Belaire <theo.belaire@gmail.com>
2013-08-24 23:07:21 -0400

    Moved boids into their own, removed other projects

--- dla3d/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-cd unoptimized-haskell; cabal clean ; cabal configure ; cabal build ; cd ..
-cd optimized-haskell1; cabal clean ; cabal configure ; cabal build ; cd ..
-cd optimized-haskell2; cabal clean ; cabal configure ; cabal build ; cd ..
-cd optimized-haskell3; cabal clean ; cabal configure ; cabal build ; cd ..
-cd optimized-haskell4; cabal clean ; cabal configure ; cabal build ; cd ..
-cd optimized-haskell5; cabal clean ; cabal configure ; cabal build ; cd ..
-cd optimized-haskell6; cabal clean ; cabal configure ; cabal build ; cd ..
-cd optimized-haskell7; cabal clean ; cabal configure ; cabal build ; cd ..
--- dla3d/c_algebra/mt-wrap.h
+++
@@ -1,7 +0,0 @@
-#ifndef __MT_WRAP_H__
-#define __MT_WRAP_H__
-
-inline void setuprandom(unsigned long long seed);
-inline double randomdouble();
-
-#endif /* __MT_WRAP_H__ */
