d599fd75005d73f469ca0f5e50980f8ad75ca6c7
LiuChen Lu <lclu@uwaterloo.ca>
2013-09-04 13:36:52 -0400

    Compilling :D

--- boids.hs
+++ boids.hs
@@ -6,19 +6,21 @@ import Debug.Trace
 import Graphics.Gloss
 import Graphics.Gloss.Data.Display
 import Graphics.Gloss.Data.Picture
-import Graphics.Gloss.Interface.IO.Simulate
-import Data.Monoid
+import Graphics.Gloss.Interface.Pure.Game
+-- import Data.Monoid.Writer
 import Control.Monad.Writer.Lazy
 import Data.Maybe  (fromJust, isJust)
+import Data.List (minimumBy,find,delete,foldl')
+import Data.Function (on)
 -- for random seed
 --import Data.Time (formatTime, getCurrentTime)
 --import System.Locale (defaultTimeLocale)
 --import Control.Applicative
 
-data Food = Food { position :: Vec2}
+data Food = Food { fposition :: Vec2} deriving (Eq)
 
 data Boid = Boid { identifier :: Int,
-                   position :: Vec2,
+                   bposition :: Vec2,
                    velocity :: Vec2,
                    hunger :: Double, -- in [0,1] if hunger reaches 1, then the Boid starves
                    cohesionScale :: Double,
@@ -68,7 +70,7 @@ velocityScale = 10.0 * (realToFrac (max (maxx-minx) (maxy-miny)) :: Float)
 -- colors
 --
 boidColor :: Boid -> Color
-boidColor b     = makeColor 1.0 1.0 0.0 1.0*(1 - hunger b)
+boidColor b     = makeColor 1.0 1.0 0.0 ( realToFrac (1-(hunger b)) )
 -- radiusColor     = makeColor 0.5 1.0 1.0 0.2
 cohesionColor :: Boid -> Color
 cohesionColor b = makeColor scaled 0.0 0.0 1.0
@@ -82,7 +84,7 @@ alignmentColor b = makeColor 0.0 0.0 scaled 1.0
 
 renderboid :: World -> Boid -> Picture
 renderboid world b =
-  let (Vec2 x y) = position b
+  let (Vec2 x y) = bposition b
       sf = 5.0 * (scaleFactor world)
       sf' = 1.0/3.0 * (scaleFactor world)
       sf''= 2.0/3.0 * (scaleFactor world)
@@ -105,17 +107,17 @@ renderboid world b =
     ]
 
 -- TODO check type signature!
-renderfoods :: World -> [Food] -> Picture
+renderfoods :: World -> [Food] -> [Picture]
 renderfoods world [] = []
 renderfoods world foods = 
   let foi : rest = foods -- food of interest : rest of food
-      (Vec2 x y) = position foi
+      (Vec2 x y) = fposition foi
       (xs,ys) = modelToScreen world (x,y)
-  in Pictures $ [
-       -- food will appear as small white squares
-       -- TODO check that squares are called squares
-       Color $ makeColor 1 1 1 1 $ Translate xs yx $ Square 2
-     ] ++ renderfoods world rest
+  in -- food will appear as small white squares
+      ( Color   (makeColor 1 1 1 1)
+             $ Translate xs ys 
+             $ Polygon [(xs-1,ys-1),(xs+1 ,ys-1),(xs+1 ,ys+1),(xs-1 ,ys+1),(xs-1,ys-1)]
+      ) : renderfoods world rest
 
 -- TODO check the type signature here :/
 renderboids :: World -> (KDTreeNode Boid , [Food]) -> Picture
@@ -143,7 +145,7 @@ initialize n sp sv = do
       makeboids (a:b:c:d:e:f:g:rest) (id:ids) =
          (Boid {identifier = id,
                 velocity = Vec2 (sv*(0.5 - a)/2.0) (sv*(0.5 - b)/2.0),
-                position = Vec2 (sp*(0.5 - c)/2.0) (sp*(0.5 - d)/2.0),
+                bposition = Vec2 (sp*(0.5 - c)/2.0) (sp*(0.5 - d)/2.0),
                 cohesionScale = maxcohesion * e,
                 separationScale = maxseparation * f,
                 alignmentScale = maxalignment * g,
@@ -153,11 +155,12 @@ initialize n sp sv = do
                 dbgA = vecZero}) : makeboids rest ids
   return $ makeboids nums [1..n]
 
-initializeFoods :: Int -> Double -> Double -> IO [Food]
+--initializeFoods :: Int -> Double -> Double -> IO [Food]
 initializeFoods numBoids scalePos = do
-    nums <- rnlist 2*(floor (n/2)) -- for 2 boids, there will be one food. Let them STARVE! muhaha
+    nums <- (rnlist $ 2*(numBoids `div `2)) -- for 2 boids, there will be one food. Let them STARVE! muhaha
     let makefoods [] = []
-        makefoods (x:y:rest) = Food {position = Vec2(sp*(0.5-a)/2.0 sp(0.5-b)/2.0)} :
+        makefoods (a:b:rest) = 
+            Food {fposition = Vec2 (scalePos*(0.5-a)/2.0) (scalePos*(0.5-b)/2.0)} :
                                makefoods rest
     return (makefoods nums)
 {--
@@ -199,8 +202,8 @@ maxcohesion = 0.0175 -- originally 0.0075
 sParam = 1.25 -- originally 1.25
 maxseparation = 0.2 -- orignally 0.1
 
-sight = 3*(scaleFactor world) :: Double
-hungerScale = 2.0 :: Double
+sight = 3*(scaleFactor world) -- :: Double
+hungerScale = 2.0  -- :: Double
 
 maxalignment = 1.5 -- orignally 1.0 / 1.8
 vLimit = 0.0025 * (max (maxx-minx) (maxy-miny))
@@ -225,9 +228,12 @@ euclidean (Vec2 x y) = sqrt ( x^2 + y^2 )
 
 seekFood :: Boid -> [Food] -> Vec2
 seekFood b foods = 
-    let p = position b
-        foodlist = map (vecSub p) foods
-        closestFood = vecSub VecZero $ minimumBy (compare `on` euclidean) foodlist
+    let p = bposition b
+        foodlist = map ((vecSub p) . fposition)  foods
+        closestFood = 
+         case foodlist of 
+          [] -> vecZero
+          _  -> vecSub vecZero $ minimumBy (compare `on` euclidean) foodlist
     in vecScale (vecScale closestFood (hunger b)) hungerScale
 
 -- three rules : cohesion (seek centroid), separation (avoid neighbors),
@@ -240,7 +246,7 @@ findCentroid :: [Boid] -> Vec2
 findCentroid []    = error "Bad centroid"
 findCentroid boids =
   let n = length boids
-  in vecScale (foldl1 vecAdd (map position boids)) (1.0 / (fromIntegral n))
+  in vecScale (foldl1 vecAdd (map bposition boids)) (1.0 / (fromIntegral n))
 
 --
 -- cohesion : go towards centroid.  parameter dictates fraction of
@@ -249,14 +255,14 @@ findCentroid boids =
 cohesion :: Boid -> [Boid] -> Double -> Vec2
 cohesion b boids a = vecScale diff a
   where c = findCentroid boids
-        p = position b
+        p = bposition b
         diff = vecSub c p
 
 -- separation.
 separation :: Boid -> [Boid] -> Double -> Vec2
 separation b []    a = vecZero
 separation b boids a =
-  let diff_positions = map (\i -> vecSub (position i) (position b)) boids
+  let diff_positions = map (\i -> vecSub (bposition i) (bposition b)) boids
       closeby = filter (\i -> (vecNorm i) < a) diff_positions
       sep = foldl vecSub vecZero closeby
   in
@@ -274,32 +280,30 @@ alignment b boids a =
 
 distance :: Vec2 -> Vec2 -> Double
 distance v1 v2 = sqrt((x1-x2)^2+(y1-y2)^2)
-    where (x1,y1)=v1
-          (x2,y2)=v2
+    where (Vec2 x1 y1)=v1
+          (Vec2 x2 y2)=v2
 
 -- one boid
-oneboid :: Writer [Food] Boid -> [Boid] -> Writer [Food] Boid
-oneboid wb boids =
-  let (b,foods) = runWriter wb
-      c = cohesion b boids (cohesionScale b)
-      s = separation b boids (separationScale b)
-      a = alignment b boids (alignmentScale b)
+oneboid :: Boid -> [Boid] -> [Food] -> (Boid,[Food])
+oneboid b boids foods=
+  let c = cohesion b boids (cohesionScale b) `vecScale` 0.1
+      s = separation b boids (separationScale b) `vecScale` 0.1
+      a = alignment b boids (alignmentScale b) `vecScale` 0.1
       f = seekFood b foods
-      p = position b
+      p = bposition b
       v = velocity b
-      v' = vecAdd (vecAdd (vecAdd v (vecScale (vecAdd c (vecAdd s a)) 0.1))
-                          (edge_repel p) 
-                   f)
+      v' = foldl' vecAdd (Vec2 0 0) [ v, c, s, a, (edge_repel p), f ]
       v'' = limiter (vecScale v' 1.0025) vLimit
       p' = vecAdd p v''
-      maybefood = find ( \foodParticle -> (distance p (position foodParticle)) <= epsilon )
+      maybefood = find ( \foodParticle -> (distance p (fposition foodParticle)) <= epsilon )
                   foods
+         -- maybefood is food that it can eat this round
       (remainingFoods,newhunger)=
-        if (isJust maybefood) 
-        then (delete (fromJust maybefood) foods, 0)
-        else (foods , (hunger b) - 0.01)
+        case  maybefood of
+         (Just food) -> (delete food foods, 0)
+         Nothing ->     (foods , (hunger b) + 0.01)
   in
-      Writer ( b { position = wraparound p',
+      (b { bposition = wraparound p',
                    velocity = v'',
                    hunger = newhunger,
                    dbgC = c,
@@ -340,7 +344,7 @@ repel x maxx minx | (x - minx) < cap =   c / (x - minx)**2
 
 findNeighbors :: KDTreeNode Boid -> Boid -> [Boid]
 findNeighbors w b =
-  let p = position b
+  let p = bposition b
 
       -- bounds
       vlo = vecSub p epsvec
@@ -352,9 +356,9 @@ findNeighbors w b =
 
       -- adjuster for wraparound
       adj1 ax ay (pos, theboid) = (vecAdd pos av,
-                                   theboid { position = vecAdd p av })
+                                   theboid { bposition = vecAdd p av })
         where av = Vec2 ax ay
-              p = position theboid
+              p = bposition theboid
 
       adjuster lo hi ax ay = let neighbors = kdtRangeSearch w lo hi
                              in map (adj1 ax ay) neighbors
@@ -408,46 +412,55 @@ growFood :: Int -> Double -> IO [Food]
 growFood n sp = do
     nums <-rnlist 2
     let grow [] = []
-        grow [a:b:rest] = 
-            Food {position=Vec2(sp*(0.5-a)/2.0 sp*(0.5-b)/2.0)} : (grow rest)
+        grow (a:b:rest) = 
+            Food {fposition=Vec2(sp*(0.5-a)/2.0) (sp*(0.5-b)/2.0)} : (grow rest)
     return $ grow nums
 
 -- writer traverse tree. modifed from mapKDTree
-wtraverseTree :: KDTreeNode a -> [Food] ->  (Writer [Food] a -> Writer [Food] b) -> ([Food],[b])
-wtraverseTree Empty _ = []
-wtraverseTree (Node Empty position boid right) foods f = 
-  (boidList,foodList)
+-- wtraverseTree :: KDTreeNode a -> [Food] ->  (Writer [Food] a -> Writer [Food] b) -> ([Food],[b])
+{-
+wtraverseTree Empty _ _ = ([],[]) -- Check this
+wtraverseTree (Node Empty pos boid right) foods f = 
+  (removedBoidList,foodlist)foods
   where
-    Writer (remainingFoods,updatedboid) = f ( Writer(boid,foods) )
-    (foodlist,boidlist) = wtraverseTree right remainingFoods f
-    boidList = if (hunger updatedboid) > 0 then updatedboid:boidlist else boidlist
-wtraverseTree (Node (Node leftl positionl boidl rightl) position boid right) foods f =
-    wtraverseTree (Node leftl positionl nodel (Node rightl position boid right)) foods f
+    (updatedboid,remainingFoods) = runWriter $ f ( writer(boid,foods) )
+    (foodlist,boidlist)          = wtraverseTree right remainingFoods f
+    removedBoidList              =if (hunger updatedboid) > 0 
+                                 then updatedboid:boidlist else boidlist
+wtraverseTree (Node (Node leftl positionl boidl rightl) pos boid right) foods f =
+    wtraverseTree (Node leftl positionl boidl (Node rightl pos boid right)) foods f
+-}
+-- :: [State [Food] Boid] -> State [Food] [Boids]
+serialize ::  [([Food] -> (Boid, [Food]))] -> [Food] -> ([Boid], [Food])
+serialize [] foods = ([], foods)
+serialize (a:as) foods = let (b, newFoods) = a foods
+                             (bs, finalFood) = serialize as newFoods in
+    (b:bs, finalFood)
 
-
-
-iterationkd :: Int -> Double -> ViewPort -> Float -> (KDTreeNode Boid,[Food]) -> (KDTreeNode Boid, [Food])
-iterationkd n sp vp _step (w,foods) = 
+-- iterationkd :: Int -> Double -> ViewPort -> Float -> (KDTreeNode Boid,[Food]) -> (KDTreeNode Boid, [Food])
+iterationkd n sp step (w,foods) = 
   let 
-    (boids,rfoods)=wtraverseTree w (\Writer(i,j) -> oneboid Writer(i,j) (findNeighbors w i))
-    newfoods = (growFood n sp) ++ rfoods
-  in (foldl (\t b -> kdtAddPoint t (position b) b) newKDTree boids, newfoods)
+    actions = map (\b -> oneboid b (findNeighbors w b)) $ kdtreeToList w
+    (boids,rfoods) = serialize actions foods
+    newfoods = (unsafePerformIO (growFood n sp)) ++ rfoods
+  in (foldl (\t b -> kdtAddPoint t (bposition b) b) newKDTree boids, newfoods)
 
 main :: IO ()
-main = do
-  let 
-    sp = 10.0
-    sv = 0.5
-    n = 100
-    bs <- initialize n sp sv -- boids starting
-    fs <- initializeFoods n sp -- foods starting
+main = 
+  let sp = 10.0
+      sv = 0.5
+      n  = 100 in do
+  bs <- initialize n sp sv -- boids starting
+  fs <- initializeFoods n sp -- foods starting
     -- tf is (tree,food)
-    tf = (foldl (\t b -> kdtAddPoint t (position b) b) newKDTree bs, -- newKDTree
+  let
+    tf = (foldl (\t b -> kdtAddPoint t (bposition b) b) newKDTree bs, -- newKDTree
           fs)
-  simulate
+  play
     (InWindow "Boids" (pixWidth world, pixHeight world) (10,10))
     (greyN 0.1)
     30
     tf
     (renderboids world)
+    (\_ w -> w)
     (iterationkd n sp)-- TODO what is happening here?? what?? is there side effects????
